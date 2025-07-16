starkware-libs/stwo

no std verifier

crates/prover/src/core/fri.rs
@@ -1,8 +1,7 @@
-use std::cmp::Reverse;
-use std::collections::{BTreeMap, BTreeSet};
-use std::fmt::Debug;
-use std::iter::zip;
-use std::ops::RangeInclusive;
+use core::cmp::Reverse;
+use core::fmt::Debug;
+use core::iter::zip;
+use core::ops::RangeInclusive;
 
 use itertools::{zip_eq, Itertools};
 use num_traits::Zero;
@@ -22,6 +21,7 @@ use crate::core::poly::line::{LineDomain, LinePoly};
 use crate::core::utils::bit_reverse_index;
 use crate::core::vcs::verifier::{MerkleDecommitment, MerkleVerificationError, MerkleVerifier};
 use crate::core::vcs::MerkleHasher;
+use crate::{vec, BTreeMap, BTreeSet, Vec};
 
 /// FRI proof config
 // TODO(andrew): Support different step sizes.
@@ -389,7 +389,7 @@ impl CirclePolyDegreeBound {
 }
 
 impl PartialOrd<LinePolyDegreeBound> for CirclePolyDegreeBound {
-    fn partial_cmp(&self, other: &LinePolyDegreeBound) -> Option<std::cmp::Ordering> {
+    fn partial_cmp(&self, other: &LinePolyDegreeBound) -> Option<core::cmp::Ordering> {
         Some(self.log_degree_bound.cmp(&other.log_degree_bound))
     }
 }

crates/prover/src/core/poly/circle/domain.rs
@@ -1,4 +1,4 @@
-use std::iter::Chain;
+use core::iter::Chain;
 
 use itertools::Itertools;
 
@@ -6,6 +6,7 @@ use crate::core::circle::{
     CirclePoint, CirclePointIndex, Coset, CosetIterator, M31_CIRCLE_LOG_ORDER,
 };
 use crate::core::fields::m31::BaseField;
+use crate::Vec;
 
 pub const MAX_CIRCLE_DOMAIN_LOG_SIZE: u32 = M31_CIRCLE_LOG_ORDER - 1;
 
@@ -116,6 +117,7 @@ mod tests {
     use super::CircleDomain;
     use crate::core::circle::{CirclePointIndex, Coset};
     use crate::core::poly::circle::CanonicCoset;
+    use crate::Vec;
 
     #[test]
     fn test_circle_domain_iterator() {

crates/prover/src/core/vcs/blake2_hash.rs
@@ -1,8 +1,10 @@
-use std::fmt;
+use core::fmt;
 
 use blake2::{Blake2s256, Digest};
 use bytemuck::{Pod, Zeroable};
 use serde::{Deserialize, Serialize};
+
+use crate::Vec;
 
 // Wrapper for the blake2s hash type.
 #[repr(C, align(32))]
@@ -101,6 +103,7 @@ mod tests {
     use blake2::Digest;
 
     use super::{Blake2sHash, Blake2sHasher};
+    use crate::ToString;
 
     impl Blake2sHasher {
         fn finalize_reset(&mut self) -> Blake2sHash {

crates/prover/src/core/vcs/poseidon252_merkle.rs
@@ -8,6 +8,7 @@ use crate::core::channel::{MerkleChannel, Poseidon252Channel};
 use crate::core::fields::m31::{BaseField, M31};
 use crate::core::vcs::hash::Hash;
 use crate::core::vcs::MerkleHasher;
+use crate::Vec;
 
 const ELEMENTS_IN_BLOCK: usize = 8;
 
@@ -33,7 +34,7 @@ impl MerkleHasher for Poseidon252MerkleHasher {
         let padded_values = column_values
             .iter()
             .copied()
-            .chain(std::iter::repeat_n(BaseField::zero(), padding_length));
+            .chain(core::iter::repeat_n(BaseField::zero(), padding_length));
         for chunk in padded_values.array_chunks::<ELEMENTS_IN_BLOCK>() {
             values.push(construct_felt252_from_m31s(&chunk));
         }
@@ -56,7 +57,7 @@ fn construct_felt252_from_m31s(word: &[M31; 8]) -> FieldElement252 {
     }
 
     let felt_bytes = [felt_as_u256[1].to_be_bytes(), felt_as_u256[0].to_be_bytes()];
-    let felt_bytes = unsafe { std::mem::transmute::<[[u8; 16]; 2], [u8; 32]>(felt_bytes) };
+    let felt_bytes = unsafe { core::mem::transmute::<[[u8; 16]; 2], [u8; 32]>(felt_bytes) };
     FieldElement252::from_bytes_be(&felt_bytes).unwrap()
 }
 

Bitsage-Network/stwo-gpu

crates/stwo/src/core/fri.rs
@@ -1,12 +1,12 @@
-use std::cmp::Reverse;
-use std::collections::{BTreeMap, BTreeSet};
-use std::fmt::Debug;
-use std::iter::zip;
-use std::ops::RangeInclusive;
+use core::cmp::Reverse;
+use core::fmt::Debug;
+use core::iter::zip;
+use core::ops::RangeInclusive;
 
 use itertools::{zip_eq, Itertools};
 use num_traits::Zero;
 use serde::{Deserialize, Serialize};
+use std_shims::{vec, BTreeMap, BTreeSet, Vec};
 use thiserror::Error;
 
 use super::channel::{Channel, MerkleChannel};
@@ -389,7 +389,7 @@ impl CirclePolyDegreeBound {
 }
 
 impl PartialOrd<LinePolyDegreeBound> for CirclePolyDegreeBound {
-    fn partial_cmp(&self, other: &LinePolyDegreeBound) -> Option<std::cmp::Ordering> {
+    fn partial_cmp(&self, other: &LinePolyDegreeBound) -> Option<core::cmp::Ordering> {
         Some(self.log_degree_bound.cmp(&other.log_degree_bound))
     }
 }

crates/stwo/src/core/poly/circle/domain.rs
@@ -1,6 +1,7 @@
-use std::iter::Chain;
+use core::iter::Chain;
 
 use itertools::Itertools;
+use std_shims::Vec;
 
 use crate::core::circle::{
     CirclePoint, CirclePointIndex, Coset, CosetIterator, M31_CIRCLE_LOG_ORDER,
@@ -112,6 +113,7 @@ type CircleDomainIndexIterator =
 #[cfg(test)]
 mod tests {
     use itertools::Itertools;
+    use std_shims::Vec;
 
     use super::CircleDomain;
     use crate::core::circle::{CirclePointIndex, Coset};

crates/stwo/src/core/vcs/blake2_hash.rs
@@ -1,8 +1,9 @@
-use std::fmt;
+use core::fmt;
 
 use blake2::{Blake2s256, Digest};
 use bytemuck::{Pod, Zeroable};
 use serde::{Deserialize, Serialize};
+use std_shims::Vec;
 
 // Wrapper for the blake2s hash type.
 #[repr(C, align(32))]
@@ -99,6 +100,7 @@ impl Blake2sHasher {
 #[cfg(test)]
 mod tests {
     use blake2::Digest;
+    use std_shims::ToString;
 
     use super::{Blake2sHash, Blake2sHasher};
 

crates/stwo/src/core/vcs/poseidon252_merkle.rs
@@ -3,6 +3,7 @@ use num_traits::Zero;
 use serde::{Deserialize, Serialize};
 use starknet_crypto::{poseidon_hash, poseidon_hash_many};
 use starknet_ff::FieldElement as FieldElement252;
+use std_shims::Vec;
 
 use crate::core::channel::{MerkleChannel, Poseidon252Channel};
 use crate::core::fields::m31::{BaseField, M31};
@@ -33,7 +34,7 @@ impl MerkleHasher for Poseidon252MerkleHasher {
         let padded_values = column_values
             .iter()
             .copied()
-            .chain(std::iter::repeat_n(BaseField::zero(), padding_length));
+            .chain(core::iter::repeat_n(BaseField::zero(), padding_length));
         for chunk in padded_values.array_chunks::<ELEMENTS_IN_BLOCK>() {
             values.push(construct_felt252_from_m31s(&chunk));
         }
@@ -56,7 +57,7 @@ fn construct_felt252_from_m31s(word: &[M31; 8]) -> FieldElement252 {
     }
 
     let felt_bytes = [felt_as_u256[1].to_be_bytes(), felt_as_u256[0].to_be_bytes()];
-    let felt_bytes = unsafe { std::mem::transmute::<[[u8; 16]; 2], [u8; 32]>(felt_bytes) };
+    let felt_bytes = unsafe { core::mem::transmute::<[[u8; 16]; 2], [u8; 32]>(felt_bytes) };
     FieldElement252::from_bytes_be(&felt_bytes).unwrap()
 }
 